3dc6f2d8b16d0f9ecbd97e9bcec410c5ef244818
tombr <tom@codicoda.fr>
2026-02-16 17:44:59 +0100

    ✨ F02 : DASHBOARD > connect datas

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -11,9 +11,27 @@ export default function Dashboard() {
     <DashboardStyled>
       <aside className="menu"></aside>
       <main>
-        <div className="info-pro">{JSON.stringify(pro)}</div>
+        <div className="info-pro">
+          <span>{pro.lastName}</span>
+          <span>{pro.firstName}</span>
+          <span>{pro.job}</span>
+          <span>{pro.mail}</span>
+          <span>{pro.phone}</span>
+          <span>{pro.society}</span>
+          <span>{pro.website}</span>
+        </div>
         <hr />
-        <div className="patients-container">{}</div>
+        <div className="patients-container">
+          {patients.map((patient) => {
+            return (
+              <div className="patient">
+                <span>{patient.lastName}</span>
+                <span>{patient.firstName}</span>
+                <span>{patient.sex}</span>
+              </div>
+            )
+          })}
+        </div>
       </main>
     </DashboardStyled>
   )
@@ -34,9 +52,18 @@ const DashboardStyled = styled.div`
     .info-pro {
       border: solid black 1px;
       height: 10vh;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
     }
     .patients-container {
       flex: 1;
+      display: flex;
+      gap: 50px;
+      .patient {
+        display: flex;
+        flex-direction: column;
+      }
     }
   }
 `
