3681e94ec8b71927f9ffd20840a401261567bb74
tombr <tom@codicoda.fr>
2026-02-16 17:35:58 +0100

    💄F02 : DASHBOARD > create layout

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,42 @@
+import { useState } from "react"
+import { fakePatients } from "../../../datas/fakePatients"
+import { fakePro } from "../../../datas/fakePro"
+import styled from "styled-components"
+
 export default function Dashboard() {
-  return <div>Dashboard</div>
+  const [patients, setPatients] = useState(fakePatients)
+  const [pro, setPro] = useState(fakePro)
+
+  return (
+    <DashboardStyled>
+      <aside className="menu"></aside>
+      <main>
+        <div className="info-pro">{JSON.stringify(pro)}</div>
+        <hr />
+        <div className="patients-container">{}</div>
+      </main>
+    </DashboardStyled>
+  )
 }
+
+const DashboardStyled = styled.div`
+  display: flex;
+  .menu {
+    height: 100vh;
+    width: 12vw;
+    border-right: solid black 1px;
+  }
+  main {
+    height: 100vh;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    .info-pro {
+      border: solid black 1px;
+      height: 10vh;
+    }
+    .patients-container {
+      flex: 1;
+    }
+  }
+`
