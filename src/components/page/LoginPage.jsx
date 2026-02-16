11f471400b3422b115c31542b56bedc1234612a6
tombr <tom@codicoda.fr>
2026-02-16 11:04:46 +0100

    💄F01: Create layout

--- src/components/page/LoginPage.jsx
+++ src/components/page/LoginPage.jsx
@@ -1,3 +1,14 @@
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  return (
+    <div>
+      <div>
+        <h1>APAPEC</h1>
+        <h2>Identifiez-vous !</h2>
+      </div>
+      <form action="submit">
+        <input type="text" placeholder="identifiant" />
+        <input type="text" placeholder="mot de passe" />
+      </form>
+    </div>
+  )
 }
