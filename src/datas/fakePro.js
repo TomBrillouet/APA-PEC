881972a636a02cbcb494c0dca606eabd5272eaf4
tombr <tom@codicoda.fr>
2026-02-16 17:23:29 +0100

    🍱F02: Add fakePro

---
+++ src/datas/fakePro.js
@@ -0,0 +1,9 @@
+export const fakePro = {
+  lastName: "Page",
+  firstName: "Sibylle",
+  mail: "sibylle.page@gmail.com",
+  phone: "0123456789",
+  website: "",
+  job: "Enseignant en activité physique adaptée (EAPA)",
+  society: "",
+}
