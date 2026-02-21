aae3717f52eb51fb96e75e962c3a177d94aea74b
tombr <tom@codicoda.fr>
2026-02-21 13:25:05 +0100

    ✏️F04: Fix name Toast component

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components"
 import Main from "./Main/Main"
 import Menu from "./Menu/Menu.jsx"
-import Toastadmin from "./Toastadmin.jsx"
+import Toast from "./Toast.jsx"
 
 export default function Dashboard() {
   return (
     <DashboardStyled>
       <Menu />
       <Main />
-      <Toastadmin />
+      <Toast />
     </DashboardStyled>
   )
 }
