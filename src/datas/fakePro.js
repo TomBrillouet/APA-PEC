05b0f90cd61f51a62cdc8aa8b24d7f5e26c66a0e
Tom <tbrillouet@tennispro.fr>
2026-02-26 19:34:31 +0100

    🔧F04 : Change initi datas

--- src/datas/fakePro.js
+++ src/datas/fakePro.js
@@ -2,8 +2,8 @@ export const fakePro = {
   lastName: "Brillouet",
   firstName: "Tom",
   mail: "tom.brillouet@outlook.fr",
-  phone: "0123456789",
-  website: "url du site https://codicoda.fr",
-  job: "Enseignant en activité physique adaptée (EAPA)",
-  society: "Sibyréeduc ",
+  phone: "0695289332",
+  website: "https://codicoda.fr",
+  job: "Développeur web",
+  society: "Codicoda ",
 }
