fa6e068a00aec4236d0f749ccc63cf5ab928f206
Tom <tbrillouet@tennispro.fr>
2026-02-27 11:37:52 +0100

    💄 🐛F04: fix block on IntermediarySection

--- src/components/pages/dashboard/Main/Popup/PatientOpened/NewBilan/BilanForm/questionsSection/IntermediarySection.jsx
+++ src/components/pages/dashboard/Main/Popup/PatientOpened/NewBilan/BilanForm/questionsSection/IntermediarySection.jsx
@@ -1,12 +1,12 @@
 import { BILAN_FIELDS } from "../../../../../../../../../datas/bilanConfig"
 import InputSection from "../../../../../../../../reusable/InputSection"
 
-export default function IntermediarySection({ mapTextAreas, block }) {
+export default function IntermediarySection({ mapTextAreas }) {
   return (
     <InputSection
       datas={mapTextAreas(BILAN_FIELDS.intermediaire.textareas)}
       label={"Développement du patient"}
-      block={block}
+      block
     />
   )
 }
