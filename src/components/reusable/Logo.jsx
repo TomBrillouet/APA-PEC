4240c40aa1d6cd288d98094f1843b09b37961908
Tom <tbrillouet@tennispro.fr>
2026-03-06 11:09:26 +0100

    🍱 F09 : MENU/ASIDE > Add logo

--- src/components/reusable/Logo.jsx
+++ src/components/reusable/Logo.jsx
@@ -1,31 +1,18 @@
 import styled from "styled-components"
-import { theme } from "../../theme"
 
-export default function Logo({ className }) {
+export default function Logo() {
   return (
     <LogoStyled>
-      <h1 className={className}>
-        APA<span>-</span>PEC
-      </h1>
+      <img src="/images/logo.png" alt="logo" />
     </LogoStyled>
   )
 }
 
 const LogoStyled = styled.div`
-  h1 {
-    font-size: 3.5rem;
-    font-weight: 800;
-    color: ${theme.colors.primary};
-    margin: 0;
-    letter-spacing: -2px;
-    line-height: 1;
-
-    span {
-      opacity: 0.2;
-    }
-
-    @media (max-width: 768px) {
-      font-size: 2.5rem;
-    }
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  img {
+    transform: scale(2.5);
   }
 `
