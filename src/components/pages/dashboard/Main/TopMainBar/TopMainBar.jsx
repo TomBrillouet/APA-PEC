51c916def0d0f55ac7ae35141a9b598d29099f24
Tom <tbrillouet@tennispro.fr>
2026-03-13 11:13:27 +0100

    💄 F14: TopMainBar component > styling of search and button elements

--- src/components/pages/dashboard/Main/TopMainBar/TopMainBar.jsx
+++ src/components/pages/dashboard/Main/TopMainBar/TopMainBar.jsx
@@ -9,20 +9,26 @@ export default function TopMainBar({ onChange, onClick, archived }) {
       <SectionTitle
         label={`Prises en charge ${archived ? "terminées" : "en cours"}`}
       />
-      <Searchbar onChange={onChange} />
-      <Button label={"Ajouter un patient"} onClick={onClick} />
+      <div className="flex-container">
+        <Searchbar onChange={onChange} />
+        <Button label={"Ajouter un patient"} onClick={onClick} />
+      </div>
     </TopMainBarStyled>
   )
 }
 
 const TopMainBarStyled = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
   font-size: 1.5rem;
   font-weight: 600;
   color: #1e2a38;
   margin-bottom: 20px;
-  gap: 10px;
+  gap: 30px;
+  .flex-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+  }
 `
