328f208dcfea335348b8bf7674f64325cf646a45
Tom <tbrillouet@tennispro.fr>
2026-03-14 16:56:31 +0100

    ✨ F15: EXPORT > BilanSection > create export template for intermediary and final bilan

--- src/components/pages/dashboard/Main/Popup/PatientOpened/OldBilan/Export/sections/BilanSection.jsx
+++ src/components/pages/dashboard/Main/Popup/PatientOpened/OldBilan/Export/sections/BilanSection.jsx
@@ -11,9 +11,39 @@ export default function BilanSection({ selectedBilan, selectedPatient, pro }) {
         {dateFr(selectedBilan.date)}
       </h2>
       <InfosPatientsSection selectedPatient={selectedPatient} />
+      {selectedBilan.type === "intermediaire" && (
+        <div>
+          <b>Évolution de la problématique / Atteinte de l'objectif:</b>
+          <p>{selectedBilan.evolution}</p>
+          <b>Ressenti du patient:</b>
+          <p>{selectedBilan.ressenti}</p>
+        </div>
+      )}
       <TestsSection className={"tests"} selectedBilan={selectedBilan} />
-      <b>Analyse professionnelle et recommandations:</b>
-      {selectedBilan.reco && <p>{selectedBilan.reco}</p>}
+
+      {selectedBilan.reco && selectedBilan.type === "initial" && (
+        <div>
+          <b>Analyse professionnelle et recommandations:</b>
+          <p>{selectedBilan.reco}</p>
+        </div>
+      )}
+      {selectedBilan.type === "intermediaire" && (
+        <div>
+          <b>Bilan et ajustement de la prise en charge</b>
+          <p>{selectedBilan.ajustement}</p>
+        </div>
+      )}
+      {selectedBilan.type === "final" && (
+        <div>
+          <b>Les objectifs de la prise en charge ont-ils été atteints ?</b>
+          <p>{selectedBilan.objectifsconclusion}</p>
+          <b>Raison de fin d'accompagnement/quelle direction pour le patient</b>
+          <p>{selectedBilan.raisonFin}</p>
+          <b>Bilan de la prise en charge et avis du patient</b>
+          <p>{selectedBilan.avis}</p>
+        </div>
+      )}
+
       <br />
       <FooterSection pro={pro} />
     </section>
